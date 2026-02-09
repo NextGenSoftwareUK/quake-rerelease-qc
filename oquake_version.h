a2ad33ab3f8dd00c68aa5b31bb0d964912d3406a
David Ellams <davidellams@hotmail.com>
2026-02-09 18:00:26 -0500

    Update oquake_version.h

--- oquake_version.h
+++ oquake_version.h
@@ -4,7 +4,7 @@
 #ifndef OQUAKE_VERSION_H
 #define OQUAKE_VERSION_H
 
-#define OQUAKE_VERSION "1.0"
+#define OQUAKE_VERSION "1.1"
 #define OQUAKE_BUILD   "1"
 #define OQUAKE_VERSION_STR "OQuake " OQUAKE_VERSION " (Build " OQUAKE_BUILD ")"
 
